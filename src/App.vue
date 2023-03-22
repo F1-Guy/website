dedba9654dd35fb3bdc6dde345d60547f8a67704
Bartol Cagalj <bartol.cagalj@gmail.com>
2023-03-22 23:16:29 +0100

    Initial layout setup Add - Navbar and About components Add - Images and logos Add - Favicon Remove - Default files Add - bootstrap, bootstrap-icons, and sass dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue";
+import About from "./components/About.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <Navbar />
+  <div class="pt-5 mt-4 d-flex flex-column justify-content-md-center align-items-center">
+    <About />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/About.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="row container">
+        <div id="jumbo" class="col py-3">
+            <h1 class="text-light justify-content-start fw-bold huge">I'm Čagalj,</h1>
+            <h1 id="ipa" class="text-light justify-content-start fw-bold huge">Bartol Čagalj</h1>
+        </div>
+        <div class="col profile">
+            <img src="../assets/images/temp.png" class="img-fluid" alt="Responsive image">
+        </div>
+    </div>
+
+    <div id="about" class="container py-3">
+        <p class="text-light text-center">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia deserunt qui quos atque ipsa illo adipisci,
+            aspernatur quaerat tempore, nihil perspiciatis quas quidem, ab labore. Atque nam doloremque repellendus dolorum! Lorem ipsum, dolor
+            sit amet consectetur adipisicing elit. Mollitia deserunt qui quos atque ipsa illo adipisci,aspernatur quaerat tempore, nihil
+            perspiciatis quas quidem, ab labore. Atque nam doloremque repellendus dolorum!
+        </p>
+    </div>
+</template>
+
+<style scoped>
+.huge {
+    font-size: 5em;
+}
+</style>
